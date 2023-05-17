aa7317f4ef360f4c88c4af966e17a9dec3bac305
farhad <rb0214192@gmail.com>
2023-05-17 14:54:00 +0430

    create products list

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import {
   Typography,
 } from "@mui/material";
 import data from "../utils/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,22 +18,27 @@ export default function Home() {
       <Typography>Products</Typography>
       <Grid container spacing={3}>
         {data.products.map((product, index) => (
-          <Grid item xs={3} key={product.slug + index}>
+          <Grid item xs={6} sm={4} md={3} key={product.slug + index}>
             <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="auto"
-                  image={product.image}
-                  alt={product.name}
-                />
-                <CardContent>
-                  <Typography>{product.name}</Typography>
-                </CardContent>
-              </CardActionArea>
+              <Link href={`product/${product.slug}`}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="auto"
+                    image={product.image}
+                    alt={product.name}
+                  />
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Link>
               <CardActions>
                 <Typography>{product.price} $</Typography>
-                <Button size="small" color="primary"> Add To Cart </Button>
+                <Button size="small" color="primary">
+                  {" "}
+                  Add To Cart{" "}
+                </Button>
               </CardActions>
             </Card>
           </Grid>
