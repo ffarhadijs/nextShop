ebdfb8b2adf53b1da493df0a0b4d2e8cfd1de475
farhad <rb0214192@gmail.com>
2023-05-29 23:07:50 +0330

    add type

--- models/User.ts
+++ models/User.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
+import { UserType } from "../types/user.type";
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
-  isAdmin: { type: Boolean, default:false },
+  isAdmin: { type: Boolean, default: false },
 });
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+const User =
+  mongoose.models.User || mongoose.model<UserType>("User", userSchema);
 
 export default User;
