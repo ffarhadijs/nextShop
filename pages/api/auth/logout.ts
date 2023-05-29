8e0fa75acc9bddf434b6a6ce87cd53f574c36b7e
farhad <rb0214192@gmail.com>
2023-05-29 23:17:27 +0330

    logout api handled

---
+++ pages/api/auth/logout.ts
@@ -0,0 +1,22 @@
+import { serialize } from "cookie";
+import { NextApiResponse, NextApiRequest } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return;
+  }
+
+  res
+    .status(200)
+    .setHeader(
+      "Set-Cookie",
+      serialize("token", "", {
+        maxAge: 0,
+        path: "/",
+      })
+    )
+    .json({ status: "success", message: "Logged out!" });
+}
