2cab7097a97a4a6c9cc762efa3e03abf2ace067a
farhad <rb0214192@gmail.com>
2023-06-06 18:47:10 +0330

    create shipping page

---
+++ pages/shipping.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const shipping = () => {
+  return (
+    <Layout>
+      <div>shipping</div>
+    </Layout>
+  );
+};
+
+export default shipping;
