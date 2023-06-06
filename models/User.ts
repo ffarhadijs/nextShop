b116553e5bf9e505eba395920d204f5d6172aa05
farhad <rb0214192@gmail.com>
2023-06-06 18:46:00 +0330

    update

--- models/User.ts
+++ models/User.ts
@@ -3,6 +3,10 @@ import { UserType } from "../types/user.type";
 
 export const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  lastName: { type: String, required: false },
+  address: { type: String, required: false },
+  postalCode: { type: Number, required: false },
+  city: { type: String, required: false },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
