243673fe58a4b71c5e66631083d64509f5ff5a53
farhad farhadi <rb0214192@gmail.com>
2023-07-30 23:40:45 +0430

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import StoreProvider from "../utils/Store";
+import Layout from "../components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StoreProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </StoreProvider>
   );
 }
