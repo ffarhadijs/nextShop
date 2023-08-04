da0c752de005293616a6c632417d267b19c70f36
farhad farhadi <rb0214192@gmail.com>
2023-08-04 19:37:57 +0430

    create component for swiper slider

---
+++ components/layout/swiper/SwiperSlider.tsx
@@ -0,0 +1,31 @@
+import { Swiper as SwiperComponent } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Swiper } from "swiper/types";
+
+export default function SwiperSlider({
+  items,
+  ...props
+}: {
+  props?: Swiper;
+  items: any[];
+}) {
+  return (
+    <SwiperComponent
+      loop={true}
+      effect="slide"
+      className="mySwiper"
+      navigation={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay, Navigation]}
+      {...props}
+    >
+      {items}
+    </SwiperComponent>
+  );
+}
