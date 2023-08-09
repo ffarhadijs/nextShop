d8e0094d8e5f9db0480357defa3d7436c772ebc8
farhad farhadi <rb0214192@gmail.com>
2023-08-09 15:34:41 +0430

    create enum for query key

---
+++ enums/queryKey.ts
@@ -0,0 +1,9 @@
+export enum QueryKey {
+  LogOut = "log-out",
+  AllOrders = "all-orders",
+  OrdersList = "orders-list",
+  ProductsList = "products-list",
+  Product = "product",
+  UsersList = "users-list",
+  getUser = "get-user",
+}
