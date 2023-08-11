b091f5e0150d82b435494d43c48ab2f63b4c34ef
farhad farhadi <rb0214192@gmail.com>
2023-08-12 02:01:58 +0430

    add toast config

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,26 +1,31 @@
-import {
-  Box,
-  Container,
-  CssBaseline,
-  ThemeProvider,
-} from "@mui/material";
+import { Box, Container, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import { ColorModeContext, useMode } from "../../utils/theme";
 import { useRouter } from "next/router";
-import { useAlert } from "../../hooks/useAlert";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Toaster } from "react-hot-toast";
 
 function Layout({ children }: { children: React.ReactNode }) {
   const { route } = useRouter();
   const [theme, colorMode] = useMode();
-  const [Alert] = useAlert();
 
   return (
     <Box>
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background:
+                  theme.palette.mode === "dark" ? "#030712" : "#e5e7eb",
+                color: theme.palette.mode === "dark" ? "#f9fafb" : "#030712",
+              },
+            }}
+          />
           <Header theme={theme} colorMode={colorMode} />
           {route === "/" || route === "/product/[slug]" ? (
             <Box sx={{ minHeight: "85vh" }}>{children}</Box>
@@ -32,7 +37,7 @@ function Layout({ children }: { children: React.ReactNode }) {
               {children}
             </Container>
           )}
-          <Footer theme={theme}/>
+          <Footer theme={theme} />
         </ThemeProvider>
       </ColorModeContext.Provider>
     </Box>
