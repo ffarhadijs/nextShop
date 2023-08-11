dac9730e047689abc67d228a6e56c4dfba840df2
farhad farhadi <rb0214192@gmail.com>
2023-08-12 02:00:22 +0430

    create api for call user data

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import User from "../../models/User";
-import { verifyToken } from "../../utils/verifyToken";
+import User from "../../../models/User";
+import { verifyToken } from "../../../utils/verifyToken";
 
 export default async function handler(
   req: NextApiRequest,
