a9775d55323a729a8e398f97c6f518d313ddbb89
farhad farhadi <rb0214192@gmail.com>
2023-08-12 02:02:28 +0430

    add react query config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,29 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import StoreProvider from "../utils/Store";
 import Layout from "../components/layout/Layout";
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
   return (
-    <StoreProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StoreProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <StoreProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </StoreProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
