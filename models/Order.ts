76e552dff1dc7e3ae49f4fccb39d7faab8f0677b
farhad farhadi <rb0214192@gmail.com>
2023-08-22 21:34:04 +0430

    some bugs fixed

--- models/Order.ts
+++ models/Order.ts
@@ -21,6 +21,7 @@ const orderSchema = new mongoose.Schema(
       address: { type: String, required: true },
       city: { type: String, required: true },
       country: { type: String, required: true },
+      postalCode: { type: String, required: true },
     },
     itemsPrice: { type: Number, required: true },
     shippingPrice: { type: Number, required: true },
