12a75a3160dbbcb70907f2ef31601aca6e1ab38a
farhad farhadi <rb0214192@gmail.com>
2023-08-24 23:45:44 +0430

    add config

--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+export const config = {
+  runtime: 'edge', // this is a pre-requisite
+  regions: ['iad1'], // only execute this function in iad1
+};
+
 export default async function connectDB() {
   try {
     if (mongoose.connections[0].readyState) return;
