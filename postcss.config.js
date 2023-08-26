c13af693c5c03dad5340db5c796874a271cf1b39
farhad farhadi <rb0214192@gmail.com>
2023-08-26 20:29:45 +0430

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,7 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-//   },
-// }
-
-
-
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    'postcss-preset-env',
-    [
-      'postcss-normalize',
-      {
-        allowDuplicates: false,
-      },
-    ],
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-    ],
-    'autoprefixer',
-  ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  },
+}
