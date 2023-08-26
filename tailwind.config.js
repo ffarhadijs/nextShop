a8bbf7a2720fbd276aa6019bedbbc9bdd6287a37
farhad farhadi <rb0214192@gmail.com>
2023-08-26 21:16:52 +0430

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
-const { join } = require('path');
+
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx,html}'),
-   ...createGlobPatternsForDependencies(__dirname),
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
 
   theme: {
